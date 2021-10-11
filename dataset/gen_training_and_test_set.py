25c0a7a3dd1106f6e0918183ba11c85a2deb63a3
Freja <frripe@gmail.com>
2021-10-11 16:41:33 +0200

    Generate more synthetic data

--- dataset/gen_training_and_test_set.py
+++ dataset/gen_training_and_test_set.py
@@ -9,29 +9,39 @@ from sys import argv
 # how big should the test set be compared to the training set
 percent = float(argv[1])
 types = ['jpg', 'png', 'bmp']
-paths = ['Blurry/', 'NoProblems/', 'synth_blurry/', 'synth_no_problems/']
+paths = os.listdir('original/jpg/')
+#paths = ['Blurry/', 'NoProblems/', 'synth_blurry/', 'synth_no_problems/']
 random.seed(a=1)
 
 def move(percent):
     for t in types:
         for path in paths:
             # amount of images in the folder
-            n = len(os.listdir('original/' + t + '/' + path))
+#            print(path)
+            n = len(os.listdir('original/' + t + '/' + path + '/'))
             tests = set()
             while len(tests) < n*percent:
                 tests.add(random.randint(1, n))
             
             trainings = set(range(1, n)) - tests
+            
+#            print("n: " + str(n))
+#            print("tests: " + str(tests))
+#            print("trainings: " + str(trainings))
 
             for im in tests:
-                src_path = 'original/' + t + '/' + path + str(im) + '.' + t
-                dst_path = 'test/' + t + '/' + path + str(im) + '.' + t
-                shutil.copy2(src_path, dst_path)
+                src_path = 'original/' + t + '/' + path + '/' + str(im) + '.' + t
+                dst_path = 'test/' + t + '/' + path + '/'
+                if not os.path.exists(dst_path):
+                    os.mkdir(dst_path)
+                shutil.copy2(src_path, dst_path + str(im) + '.' + t)
 
             for im in trainings:
-                src_path = 'original/' + t + '/' + path + str(im) + '.' + t
-                dst_path = 'training/' + t + '/' + path + str(im) + '.' + t
-                shutil.copy2(src_path, dst_path)
+                src_path = 'original/' + t + '/' + path + '/' + str(im) + '.' + t
+                dst_path = 'training/' + t + '/' + path + '/'
+                if not os.path.exists(dst_path):
+                    os.mkdir(dst_path)
+                shutil.copy2(src_path, dst_path + str(im) + '.' + t)
     
 move(percent)
 
--- dataset/original/gen_synth.py
+++ dataset/original/gen_synth.py
@@ -1,5 +1,5 @@
 # run from dataset:
-# python3 gen_synth.py 2
+# python3 original/gen_synth.py 2
 """
  * https://datacarpentry.org/image-processing/06-blurring/
  * Python script to demonstrate Gaussian blur.
@@ -14,27 +14,10 @@ import numpy as np
 from matplotlib import pyplot as plt
 from PIL import Image, ImageOps
 
-# get filename and kernel size from command line
-#filename = sys.argv[1]
-#sigma = float(sys.argv[2])
-#sigma_x = float(sys.argv[3])
-#
-# read and display original image
-#image = skimage.io.imread(fname=filename)
-#
-# apply Gaussian blur, creating a new image
-#blurred = skimage.filters.gaussian(
-#    image, sigma=(sigma, sigma), truncate=3.5, multichannel=True
-#)
-#
-# display blurred image
-#images = [image, blurred]
-#skimage.io.imshow_collection(images)
-#skimage.io.show()
-
+# get kernel size from command line
 s = float(sys.argv[1])
 r = 15
-types = ['jpg', 'png', 'bmp']
+types = ['png']#['jpg', 'png', 'bmp']
 
 # get truncate value for desired radius (should be odd integer)
 #radius = int(truncate * sigma + 0.5):          https://github.com/scipy/scipy/blob/c7ba5c0b239f10478d3c902cf4057edf1880b35d/scipy/ndimage/filters.py#L179
@@ -45,36 +28,46 @@ def get_truncate(sigma, radius):
 def run_mirror(fr, to, folder, synth_folder):
     for i in range(fr, to):
         for t in types:
-            filename = t + '/' + folder + '/' + str(i) + '.' + t
+            filename = 'original/' + t + '/' + folder + '/' + str(i) + '.' + t
             image = Image.open(filename)
             image_mir = ImageOps.mirror(image)
             image_flip = ImageOps.flip(image)
             image_flip_mir = ImageOps.flip(image_mir)
-            image_mir.save(t + '/' + synth_folder + '/' + str(i) + '.' + t, quality=95)
-            image_flip.save(t + '/' + synth_folder + '/' + str(i+to-1) + '.' + t, quality=95)
-            image_flip_mir.save(t + '/' + synth_folder + '/' + str(i+to*2-2) + '.' + t, quality=95)
+            image_mir.save('original/' + t + '/' + synth_folder + '/' + str(i) + '.' + t)
+            image_flip.save('original/' + t + '/' + synth_folder + '/' + str(i+(to-1)) + '.' + t)
+            image_flip_mir.save('original/' + t + '/' + synth_folder + '/' + str(i+(to-1)*2) + '.' + t)
+            for a in(1, 3):
+                image_rot1 = image.rotate(5)
+                image_rot2 = image.rotate(-5)
+                image_rot1.save('original/' + t + '/' + synth_folder + '/' + str(i+(to-1)*3) + '.' + t)
+                image_rot2.save('original/' + t + '/' + synth_folder + '/' + str(i+(to-1)*4) + '.' + t)
+                image_rot1 = image.rotate(2*5)
+                image_rot2 = image.rotate(-2*5)
+                image_rot1.save('original/' + t + '/' + synth_folder + '/' + str(i+(to-1)*5) + '.' + t)
+                image_rot2.save('original/' + t + '/' + synth_folder + '/' + str(i+(to-1)*6) + '.' + t)
 
 # apply blur to all images with no problems
-def run_gaussian(sigma, truncate):#, fr1, to1, name1, name2, folder='NoProblems', synth_folder='synth_no_problems'):
+def run_gaussian(sigma, truncate):
+    print("gauss: ", sigma)
     folder = 'NoProblems'
     synth_folder = 'synth_no_problems'
     for t in types:
-        path = t + '/synth_blurry_' + str(sigma) + '/'
+        print(t)
+        path = 'original/' + t + '/synth_blurry_' + str(sigma) + '/'
         if not os.path.exists(path):
             os.mkdir(path)
-        n1 = len(os.listdir(t + '/' + folder + '/'))
+        n1 = len(os.listdir('original/' + t + '/' + folder + '/'))
         for i in range(1, n1+1):
-            filename = t + '/' + folder + '/' + str(i) + '.' + t
+            filename = 'original/' + t + '/' + folder + '/' + str(i) + '.' + t
             image = skimage.io.imread(fname=filename)
             blurred = (skimage.filters.gaussian(
                 image, sigma=(sigma, sigma), truncate=truncate, multichannel=True
             ) * 255).astype(np.uint8)
             skimage.io.imsave(path + str(i) + '.' + t, blurred, check_contrast=False)
 
-        n2 = len(os.listdir(t + '/' + synth_folder + '/'))
+        n2 = len(os.listdir('original/' + t + '/' + synth_folder + '/'))
         for i in range(1, n2+1):
-            print(n1, n2, i)
-            filename = t + '/' + synth_folder + '/' + str(i) + '.' + t
+            filename = 'original/' + t + '/' + synth_folder + '/' + str(i) + '.' + t
             image = skimage.io.imread(fname=filename)
             blurred = (skimage.filters.gaussian(
                 image, sigma=(sigma, sigma), truncate=truncate, multichannel=True
@@ -82,9 +75,9 @@ def run_gaussian(sigma, truncate):#, fr1, to1, name1, name2, folder='NoProblems'
             skimage.io.imsave(path + str(i+n1) + '.' + t, blurred, check_contrast=False)
 
 # run
-run_mirror(1, 48, 'Blurry',            'synth_blurry')
-#run_mirror(1, 52, 'Earwax',            'synth_earwax')
-run_mirror(1, 35, 'NoProblems',        'synth_no_problems')
+run_mirror(1, 48, 'Blurry',     'synth_blurry')
+#run_mirror(1, 52, 'Earwax',   'synth_earwax')
+run_mirror(1, 35, 'NoProblems', 'synth_no_problems')
 #run_mirror(1, 21, 'NoVisibleMembrane', 'synth_no_visible_membrane')
 run_gaussian(s,     get_truncate(s, r))
 run_gaussian(s*1.5, get_truncate(s*2, r))
